1615760ddb9880e87daf7e31976c52d48c3a88d1
asifmohammed786 <asifmohammed85113@gmail.com>
2026-01-09 20:12:15 +0530

    React setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Classroom, Lab & Hall Booking System</h1>
+      <p>React frontend setup using Vite is successful.</p>
+    </div>
+  );
+}
+
+export default App;
